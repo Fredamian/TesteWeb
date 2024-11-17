a6609b9ac41e10d5a08a108682b1f58e3b10dc5d
Fredamian <fdgomes@uta.cv>
2024-11-17 21:32:21 -0100

    up date

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/TesteWeb/', // O nome do repositório no GitHub
   build: {
-    outDir: 'build', // Usando uma pasta personalizada como 'build'
+    outDir: 'build',
   },
 })
